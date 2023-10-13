2fefa955b89349264d56e36df303b67c603669d2
Andrey Miskov <amiskov@gmail.com>
2023-10-13 07:22:41 +0300

    remove recursion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-from dataclasses import dataclass
 
 import backoff
 import requests
@@ -16,15 +15,6 @@ class DevmanValueError(ValueError):
 LONG_POLLING_URL = 'https://dvmn.org/api/long_polling/'
 BACKOFF_EXCEPTIONS = (requests.ReadTimeout, requests.ConnectionError,
                       requests.Timeout, DevmanValueError)
-
-
-@dataclass
-class PollingConf:
-    url: str
-    bot: telegram.Bot
-    chat_id: int
-    headers: dict
-    timeout: int = 60
 
 
 def main():
@@ -45,56 +35,57 @@ def main():
         level=logging.INFO
     )
 
-    conf = PollingConf(
+    start_polling(
         url=LONG_POLLING_URL,
         bot=telegram.Bot(token=tg_bot_token),
         chat_id=tg_chat_id,
         headers={'Authorization': f'Token {token}'},
     )
-    start_polling(conf)
 
 
 @backoff.on_exception(backoff.expo, BACKOFF_EXCEPTIONS)
-def start_polling(conf: PollingConf, params=None):
-    resp = requests.get(conf.url, headers=conf.headers, timeout=conf.timeout)
-    resp.raise_for_status()
-    reviews = resp.json()
+def start_polling(url, bot, chat_id, headers, timeout=60):
+    params = None
+    while True:
+        resp = requests.get(url, headers=headers,
+                            params=params, timeout=timeout)
+        resp.raise_for_status()
+        reviews = resp.json()
 
-    if 'status' not in reviews:
-        msg = 'Reviews status not found in server response.'
-        logging.error(msg)
-        raise DevmanValueError(msg)
+        if 'status' not in reviews:
+            msg = 'Reviews status not found in server response.'
+            logging.error(msg)
+            raise DevmanValueError(msg)
 
-    if reviews['status'] == 'found':
-        attempt = reviews['new_attempts'][0]
+        if reviews['status'] == 'found':
+            attempt = reviews['new_attempts'][0]
 
-        is_approved = not attempt['is_negative']
-        title = attempt['lesson_title']
-        url = attempt['lesson_url']
+            is_approved = not attempt['is_negative']
+            title = attempt['lesson_title']
+            url = attempt['lesson_url']
 
-        msg = ''
-        if is_approved:
-            msg = f'✅ Урок «<a href="{url}">{title}</a>» принят!'
+            msg = ''
+            if is_approved:
+                msg = f'✅ Урок «<a href="{url}">{title}</a>» принят!'
+            else:
+                msg = f'🛠 По уроку «<a href="{url}">{title}</a>» есть замечания.'
+
+            bot.send_message(
+                chat_id=chat_id,
+                text=msg, parse_mode=PARSEMODE_HTML,
+                disable_web_page_preview=True,
+            )
+
+            params = None
+        elif reviews['status'] == 'timeout':
+            params = {
+                'timestamp': reviews['timestamp_to_request']
+            }
+            continue
         else:
-            msg = f'🛠 По уроку «<a href="{url}">{title}</a>» есть замечания.'
-
-        conf.bot.send_message(
-            chat_id=conf.chat_id,
-            text=msg, parse_mode=PARSEMODE_HTML,
-            disable_web_page_preview=True,
-        )
-
-        return start_polling(conf)
-
-    if reviews['status'] == 'timeout':
-        params = {
-            'timestamp': reviews['timestamp_to_request']
-        }
-        return start_polling(conf, params=params)
-
-    msg = f'Bad reviews status: {reviews["status"]}'
-    logging.error(msg)
-    raise DevmanValueError(msg)
+            msg = f'Bad reviews status: {reviews["status"]}'
+            logging.error(msg)
+            raise DevmanValueError(msg)
 
 
 if __name__ == '__main__':
